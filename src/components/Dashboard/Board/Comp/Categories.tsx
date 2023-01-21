fc4b0c2e701c37f7454ec80018f6b09dcd9f664c
sammyalwayscode <olorundasamuel2@gmail.com>
2023-01-21 17:43:26 +0100

    pod

--- src/components/Dashboard/Board/Comp/Categories.tsx
+++ src/components/Dashboard/Board/Comp/Categories.tsx
@@ -1,10 +1,76 @@
+import { url } from "inspector";
 import React from "react";
+import { BiMusic } from "react-icons/bi";
 import styled from "styled-components";
 
 const Categories = () => {
   return (
     <Container>
-      <Wrapper>Catrgories</Wrapper>
+      <Wrapper>
+        <Title>Explore Our Various Categouries</Title>
+        <CategoriesCardHold>
+          <CategoryCard>
+            <Cover style={{ backgroundImage: `url("/image/zxx.jpg")` }}>
+              <CoverIcon>
+                <BiMusic />
+              </CoverIcon>
+            </Cover>
+            <CategoryTitle>
+              <div>Music</div>
+            </CategoryTitle>
+          </CategoryCard>
+          <CategoryCard>
+            <Cover style={{ backgroundImage: `url("/image/zxx.jpg")` }}>
+              <CoverIcon>
+                <BiMusic />
+              </CoverIcon>
+            </Cover>
+            <CategoryTitle>
+              <div>Music</div>
+            </CategoryTitle>
+          </CategoryCard>
+          <CategoryCard>
+            <Cover style={{ backgroundImage: `url("/image/zxx.jpg")` }}>
+              <CoverIcon>
+                <BiMusic />
+              </CoverIcon>
+            </Cover>
+            <CategoryTitle>
+              <div>Music</div>
+            </CategoryTitle>
+          </CategoryCard>
+          <CategoryCard>
+            <Cover style={{ backgroundImage: `url("/image/zxx.jpg")` }}>
+              <CoverIcon>
+                <BiMusic />
+              </CoverIcon>
+            </Cover>
+            <CategoryTitle>
+              <div>Music</div>
+            </CategoryTitle>
+          </CategoryCard>
+          <CategoryCard>
+            <Cover style={{ backgroundImage: `url("/image/zxx.jpg")` }}>
+              <CoverIcon>
+                <BiMusic />
+              </CoverIcon>
+            </Cover>
+            <CategoryTitle>
+              <div>Music</div>
+            </CategoryTitle>
+          </CategoryCard>
+          <CategoryCard>
+            <Cover style={{ backgroundImage: `url("/image/zxx.jpg")` }}>
+              <CoverIcon>
+                <BiMusic />
+              </CoverIcon>
+            </Cover>
+            <CategoryTitle>
+              <div>Music</div>
+            </CategoryTitle>
+          </CategoryCard>
+        </CategoriesCardHold>
+      </Wrapper>
     </Container>
   );
 };
@@ -34,4 +100,60 @@ const Wrapper = styled.div`
   @media (max-width: 1150px) {
     width: 95%;
   }
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 800;
+  margin: 10px 0;
+`;
+const CategoriesCardHold = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+const CategoryCard = styled.div`
+  height: 270px;
+  width: 200px;
+  background-color: #0e0e0e;
+  border-radius: 4px;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+`;
+
+const Cover = styled.div`
+  height: 162px;
+  width: 162px;
+  background-color: #1f1f1f;
+  margin-top: 17px;
+  background-position: center;
+  background-size: cover;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const CoverIcon = styled.div`
+  height: 100%;
+  width: 100%;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 60px;
+`;
+
+const CategoryTitle = styled.div`
+  margin: 10px;
+  font-size: 20px;
+  font-weight: 700;
+  /* background-color: red; */
+  width: 162px;
 `;
--- src/components/Dashboard/Board/Comp/Library.tsx
+++ src/components/Dashboard/Board/Comp/Library.tsx
@@ -4,7 +4,46 @@ import styled from "styled-components";
 const Library = () => {
   return (
     <Container>
-      <Wrapper>Library</Wrapper>
+      <Wrapper>
+        <Title>Your Libuary</Title>
+        <LibraryHold>
+          <LibraryCard>
+            <LibuaryCover>
+              <img src="" alt="" />
+            </LibuaryCover>
+            <LibuaryTitle>Insprational Podcast</LibuaryTitle>
+            <LibuarySub>By Olorunda Samuel</LibuarySub>
+          </LibraryCard>
+          <LibraryCard>
+            <LibuaryCover>
+              <img src="" alt="" />
+            </LibuaryCover>
+            <LibuaryTitle>Insprational Podcast</LibuaryTitle>
+            <LibuarySub>By Olorunda Samuel</LibuarySub>
+          </LibraryCard>
+          <LibraryCard>
+            <LibuaryCover>
+              <img src="" alt="" />
+            </LibuaryCover>
+            <LibuaryTitle>Insprational Podcast</LibuaryTitle>
+            <LibuarySub>By Olorunda Samuel</LibuarySub>
+          </LibraryCard>
+          <LibraryCard>
+            <LibuaryCover>
+              <img src="" alt="" />
+            </LibuaryCover>
+            <LibuaryTitle>Insprational Podcast</LibuaryTitle>
+            <LibuarySub>By Olorunda Samuel</LibuarySub>
+          </LibraryCard>
+          <LibraryCard>
+            <LibuaryCover>
+              <img src="" alt="" />
+            </LibuaryCover>
+            <LibuaryTitle>Insprational Podcast</LibuaryTitle>
+            <LibuarySub>By Olorunda Samuel</LibuarySub>
+          </LibraryCard>
+        </LibraryHold>
+      </Wrapper>
     </Container>
   );
 };
@@ -34,4 +73,45 @@ const Wrapper = styled.div`
   @media (max-width: 1150px) {
     width: 95%;
   }
+`;
+
+const Title = styled.div`
+  font-size: 20px;
+  font-weight: 800;
+  margin: 10px 0;
+`;
+const LibraryHold = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+const LibraryCard = styled.div`
+  height: 270px;
+  width: 200px;
+  background-color: #0e0e0e;
+  border-radius: 4px;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+`;
+const LibuaryCover = styled.div`
+  height: 162px;
+  width: 162px;
+  background-color: #1f1f1f;
+  margin-top: 17px;
+`;
+const LibuaryTitle = styled.div`
+  margin: 10px;
+  font-size: 13px;
+  font-weight: 700;
+  /* background-color: red; */
+  width: 162px;
+`;
+const LibuarySub = styled.div`
+  width: 162px;
+  font-size: 10px;
+  color: #a7a79b;
 `;
